687050969078862177f70ad62a40c5520d73f0e2
wangml-start <774354431@qq.com>
2020-09-28 23:56:32 +0800

    主页面建设

--- app/src/main/java/com/cgmn/msxl/ac/AppMainActivity.java
+++ app/src/main/java/com/cgmn/msxl/ac/AppMainActivity.java
@@ -1,40 +1,37 @@
 package com.cgmn.msxl.ac;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.view.View;
-import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import com.cgmn.msxl.R;
-import com.cgmn.msxl.application.GlobalTreadPools;
-import com.cgmn.msxl.comp.CustmerToast;
-import com.cgmn.msxl.db.AppSqlHelper;
-import com.cgmn.msxl.server_interface.BaseData;
-import com.cgmn.msxl.service.OkHttpClientManager;
-import com.cgmn.msxl.service.PropertyService;
-import com.cgmn.msxl.utils.CommonUtil;
-import com.cgmn.msxl.utils.FixStringBuffer;
+import com.cgmn.msxl.comp.PageFragment;
 import com.cgmn.msxl.utils.MessageUtil;
-import com.google.gson.Gson;
-import com.squareup.okhttp.Request;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class AppMainActivity extends AppCompatActivity
         implements View.OnClickListener {
     private static final String TAG = AppMainActivity.class.getSimpleName();
     private Context mContxt;
+    //UI Object
+    private TextView txt_topbar;
+    private TextView txt_channel;
+    private TextView txt_message;
+    private TextView txt_better;
+    private TextView txt_setting;
+    private FrameLayout ly_content;
+
     //消息处理
     private Handler mHandler;
 
-    Button bt_post;
-    Button bt_get;
-    TextView display;
+    //Fragment Object
+    private PageFragment fg1,fg2,fg3,fg4;
+    private FragmentManager fManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,16 +39,32 @@ public class AppMainActivity extends AppCompatActivity
         setContentView(R.layout.app_main_layout);
         initMessageHandle();
         bindAppMainView();
+        fManager = getFragmentManager();
+        txt_channel.performClick();
     }
 
 
     private void bindAppMainView() {
         mContxt = this;
-        bt_post = findViewById(R.id.bt_post);
-        bt_get = findViewById(R.id.bt_get);
-        display = findViewById(R.id.lb_display);
-        bt_post.setOnClickListener(this);
-        bt_get.setOnClickListener(this);
+        txt_topbar = (TextView) findViewById(R.id.txt_topbar);
+        txt_channel = (TextView) findViewById(R.id.txt_channel);
+        txt_message = (TextView) findViewById(R.id.txt_message);
+        txt_better = (TextView) findViewById(R.id.txt_better);
+        txt_setting = (TextView) findViewById(R.id.txt_setting);
+        ly_content = (FrameLayout) findViewById(R.id.ly_content);
+
+        txt_channel.setOnClickListener(this);
+        txt_message.setOnClickListener(this);
+        txt_better.setOnClickListener(this);
+        txt_setting.setOnClickListener(this);
+    }
+
+    //重置所有文本的选中状态
+    private void setSelected(){
+        txt_channel.setSelected(false);
+        txt_message.setSelected(false);
+        txt_better.setSelected(false);
+        txt_setting.setSelected(false);
     }
 
     private void initMessageHandle() {
@@ -59,93 +72,68 @@ public class AppMainActivity extends AppCompatActivity
             @Override
             public boolean handleMessage(Message msg) {
                 if (msg.what == MessageUtil.REQUEST_SUCCESS) {
-                    BaseData data = (BaseData) msg.obj;
-                    Gson g = new Gson();
-                    display.setText(g.toJson(data));
                 } else if (msg.what == MessageUtil.EXCUTE_EXCEPTION) {
-                    Exception exception = (Exception) msg.obj;
-                    FixStringBuffer mes = new FixStringBuffer();
-                    mes.append("登陆失败: %s", exception.getMessage());
-                    //TODO: 异常处理
-                    CustmerToast.makeText(mContxt, mes.toString()).show();
                 }
                 return false;
             }
         });
     }
 
+    //隐藏所有Fragment
+    private void hideAllFragment(FragmentTransaction fragmentTransaction){
+        if(fg1 != null)fragmentTransaction.hide(fg1);
+        if(fg2 != null)fragmentTransaction.hide(fg2);
+        if(fg3 != null)fragmentTransaction.hide(fg3);
+        if(fg4 != null)fragmentTransaction.hide(fg4);
+    }
+
+
     @Override
     public void onClick(View v) {
-        if (v.getId() == R.id.bt_get) {
-            CustmerToast.makeText(mContxt, R.string.logining, CustmerToast.LENGTH_LONG).show();
-            GlobalTreadPools.getInstance(mContxt).execute(new Runnable() {
-                @Override
-                public void run() {
-                    AppSqlHelper sqlHeper = new AppSqlHelper(mContxt);
-                    Map<String, Object> map = sqlHeper.getActiveUser();
-                    String email = (String) map.get("phone");
-                    String password = (String) map.get("password");
-                    Map<String, String> p = new HashMap<>();
-                    p.put("pws", password);
-                    p.put("email", email);
-                    p.put("GENERAL_LOGIN", "1");
-                    String url = CommonUtil.buildGetUrl(
-                            PropertyService.getInstance().getKey("serverUrl"),
-                            "/user/login", p);
-                    OkHttpClientManager.getAsyn(url,
-                            new OkHttpClientManager.ResultCallback<BaseData>() {
-                                @Override
-                                public void onError(Request request, Exception e) {
-                                    Message message = Message.obtain();
-                                    message.what = MessageUtil.EXCUTE_EXCEPTION;
-                                    message.obj = e;
-                                    mHandler.sendMessage(message);
-                                }
-                                @Override
-                                public void onResponse(BaseData data) {
-                                    Message message = Message.obtain();
-                                    message.what = MessageUtil.REQUEST_SUCCESS;
-                                    message.obj = data;
-                                    mHandler.sendMessage(message);
-                                }
-                            });
+        FragmentTransaction fTransaction = fManager.beginTransaction();
+        hideAllFragment(fTransaction);
+        switch (v.getId()){
+            case R.id.txt_channel:
+                setSelected();
+                txt_channel.setSelected(true);
+                if(fg1 == null){
+                    fg1 = new PageFragment("第一个Fragment");
+                    fTransaction.add(R.id.ly_content,fg1);
+                }else{
+                    fTransaction.show(fg1);
                 }
-            });
-        } else {
-            CustmerToast.makeText(mContxt, R.string.logining, CustmerToast.LENGTH_LONG).show();
-            GlobalTreadPools.getInstance(mContxt).execute(new Runnable() {
-                @Override
-                public void run() {
-                    AppSqlHelper sqlHeper = new AppSqlHelper(mContxt);
-                    Map<String, Object> map = sqlHeper.getActiveUser();
-                    String email = (String) map.get("phone");
-                    String password = (String) map.get("password");
-                    OkHttpClientManager.Param[] params = new OkHttpClientManager.Param[]{
-                            new OkHttpClientManager.Param("pws", password),
-                            new OkHttpClientManager.Param("GENERAL_LOGIN", "1"),
-                            new OkHttpClientManager.Param("email", email)};
-                    String url = String.format("%s%s",
-                            PropertyService.getInstance().getKey("serverUrl"), "/user/login");
-                    OkHttpClientManager.postAsyn(url,
-                            new OkHttpClientManager.ResultCallback<BaseData>() {
-                                @Override
-                                public void onError(Request request, Exception e) {
-                                    Message message = Message.obtain();
-                                    message.what = MessageUtil.EXCUTE_EXCEPTION;
-                                    message.obj = e;
-                                    mHandler.sendMessage(message);
-                                }
-                                @Override
-                                public void onResponse(BaseData data) {
-                                    Message message = Message.obtain();
-                                    message.what = MessageUtil.REQUEST_SUCCESS;
-                                    message.obj = data;
-                                    mHandler.sendMessage(message);
-                                }
-                            },
-                            params);
+                break;
+            case R.id.txt_message:
+                setSelected();
+                txt_message.setSelected(true);
+                if(fg2 == null){
+                    fg2 = new PageFragment("第二个Fragment");
+                    fTransaction.add(R.id.ly_content,fg2);
+                }else{
+                    fTransaction.show(fg2);
                 }
-            });
+                break;
+            case R.id.txt_better:
+                setSelected();
+                txt_better.setSelected(true);
+                if(fg3 == null){
+                    fg3 = new PageFragment("第三个Fragment");
+                    fTransaction.add(R.id.ly_content,fg3);
+                }else{
+                    fTransaction.show(fg3);
+                }
+                break;
+            case R.id.txt_setting:
+                setSelected();
+                txt_setting.setSelected(true);
+                if(fg4 == null){
+                    fg4 = new PageFragment("第四个Fragment");
+                    fTransaction.add(R.id.ly_content,fg4);
+                }else{
+                    fTransaction.show(fg4);
+                }
+                break;
         }
+        fTransaction.commit();
     }
 }
