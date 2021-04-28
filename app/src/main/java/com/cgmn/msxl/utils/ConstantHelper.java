19cea5b2330e6a702f00e456723c42a18ecf015c
wml <774354431@qq.com>
2021-04-28 23:52:40 +0800

    优化

--- app/src/main/java/com/cgmn/msxl/utils/ConstantHelper.java
+++ app/src/main/java/com/cgmn/msxl/utils/ConstantHelper.java
@@ -1,13 +1,13 @@
 package com.cgmn.msxl.utils;
 
 public class ConstantHelper {
-    public static final String serverUrl = "http://192.168.1.4:8070";
+//    public static final String serverUrl = "http://192.168.1.4:8070";
 //    public static final String environment = "dev";
 
 //    public static final String serverUrl = "http://139.155.38.239:8070";
-//    public static final String serverUrl = "http://1.116.146.197:80";
+    public static final String serverUrl = "http://1.116.146.197:80";
     //支付宝支付时用
     public static final String environment = "prod";
 
-    public static final String version = "3.6.4";
+    public static final String version = "3.7.1";
 }
